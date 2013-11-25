ea1ba2eaea517f9e79965204b136f78e6e1ba879
Han-Yan <Han_Yan@mckinsey.com>
2013-11-25 01:18:11 -0500

    Update stockPriceHelper.cpp

--- source/stockPriceHelper.cpp
+++ source/stockPriceHelper.cpp
@@ -2,24 +2,37 @@
 
 const std::string StockPriceHelper::PREFIXURL = "http://finance.google.com/finance/info?client=ig&q=";
 
+/**
+ * Convert string to double
+ */
 double StockPriceHelper::s_to_d(const std::string& strPrice) {
     return std::atof(strPrice.c_str());
 }
 
+/**
+ * Retrieve real time stock price.
+ * @param  stockName name of the stock
+ * @return stock price
+ */
 double StockPriceHelper::getPrice(const std::string& stockName) {
     std::string targetUrl = PREFIXURL;
     targetUrl.append(stockName);
+    //Send a http request.
     RestClient::response r = RestClient::get(targetUrl);
+    //Get the http response.
     std::string responseJson = r.body;
 
+
+    //trim stock data in order to fit in json object.
     responseJson.erase(responseJson.begin(), responseJson.begin() + 5);
     responseJson.erase(responseJson.end() - 2, responseJson.end());
 
-    Json::Value root;
+    Json::Value root; //will contains the root value(json object) after parsing.
     Json::Reader reader;
     bool parsedSuccess = reader.parse(responseJson, root, false);
 
     if (not parsedSuccess) {
+        // report to the user the failure and their locations in the document.
         std::cout << "Failed to parse JSON" << std::endl << reader.getFormatedErrorMessages() << std::endl;
         exit(1);
     }
@@ -27,6 +40,7 @@ double StockPriceHelper::getPrice(const std::string& stockName) {
     // Print response in styled json.
     // std::cout << root.toStyledString() << std::endl;
 
+    //Get the value of the member of root named 'l_fix'(stock price).
     const Json::Value stockPrice = root["l_fix"];
 
     if (not stockPrice.isNull()) {
